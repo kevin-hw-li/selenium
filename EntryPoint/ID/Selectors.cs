a7b14bd9b3080e8508eba5c411a2a4a13ab8c24f
kevin-hw-li <hoi.li@unswalumni.com>
2017-07-03 00:50:03 +1000

    Added try and catch blocks for NoSuchElementException error

--- EntryPoint/ID/Selectors.cs
+++ EntryPoint/ID/Selectors.cs
@@ -1,77 +1,97 @@
-﻿﻿﻿﻿using System;
+﻿using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 class Selectors
 {
     static void Main()
-	{
+    {
         //declaring URL variables
         string nameUrl = "http://testing.todorvachev.com/selectors/name";
-		string idUrl = "http://testing.todorvachev.com/selectors/id";
-		string classUrl = "http://testing.todorvachev.com/selectors/class-name";
+        string idUrl = "http://testing.todorvachev.com/selectors/id";
+        string classUrl = "http://testing.todorvachev.com/selectors/class-name";
         string cssPathUrl = "http://testing.todvachev.com/selectors/css-path/";
-
+       
         //open a testing browser
-		IWebDriver driver = new ChromeDriver();
+        IWebDriver driver = new ChromeDriver();
 
         //search and test element by Name
-		driver.Navigate().GoToUrl(nameUrl);
-		IWebElement nameElement = driver.FindElement(By.Name("myName"));
-		if (nameElement.Displayed)
-		{
-			GreenMessage("The element searched by Name is displayed on the page.");
-		}
-		else
-		{
-			RedMessage("Something went wrong. The element searched by Name is not displayed.");
-		}
+        driver.Navigate().GoToUrl(nameUrl);
+        IWebElement nameElement;
 
-		//search and test element by ID
-		driver.Navigate().GoToUrl(idUrl);
-        IWebElement idElement = driver.FindElement(By.Id("testImage"));
-		if (idElement.Displayed)
+        try
+        {
+            nameElement = driver.FindElement(By.Name("myName"));
+
+            if (nameElement.Displayed)
+            {
+                GreenMessage("The element searched by Name is displayed on the page.");
+            }
+        }
+        catch (NoSuchElementException)
+        {
+            RedMessage("Something went wrong. The element searched by Name is not displayed.");
+        }
+
+        //search and test element by ID
+        driver.Navigate().GoToUrl(idUrl);
+        IWebElement idElement;
+
+		try
 		{
-			GreenMessage("The element searched by ID is displayed on the page.");
+			idElement = driver.FindElement(By.Id("testImage"));
+
+			if (idElement.Displayed)
+			{
+				GreenMessage("The element searched by ID is displayed on the page.");
+			}
 		}
-		else
+		catch (NoSuchElementException)
 		{
 			RedMessage("Something went wrong. The element searched by ID is not displayed.");
 		}
 
-		//search and test element by Class
-		driver.Navigate().GoToUrl(classUrl);
-		IWebElement classElement = driver.FindElement(By.ClassName("testClass"));
+        //search and test element by Class
+        driver.Navigate().GoToUrl(classUrl);
+        IWebElement classElement = driver.FindElement(By.ClassName("testClass"));
         Console.WriteLine("Inner Text of the testing element is:");
         GreenMessage(classElement.Text);
 
-		//search and test element by CSS Path
-		driver.Navigate().GoToUrl(cssPathUrl);
-		IWebElement CssPathElement = driver.FindElement(By.CssSelector("#post-108 > div > figure > img"));
-		if (CssPathElement.Displayed)
+        //search and test element by CSS Path
+        driver.Navigate().GoToUrl(cssPathUrl);
+        IWebElement CssPathElement;
+
+		try
 		{
-			GreenMessage("The element searched by CSS Path is displayed on the page.");
+            CssPathElement = driver.FindElement(By.CssSelector("#post-108 > div > figure > img"));
+            //to test the catch block, use the below:
+			//CssPathElement = driver.FindElement(By.CssSelector("#post-108 > div > figure > div"));
+
+			if (CssPathElement.Displayed)
+			{
+				GreenMessage("The element searched by CSS Path is displayed on the page.");
+			}
 		}
-		else
+		catch (NoSuchElementException)
 		{
 			RedMessage("Something went wrong. The element searched by CSS Path is not displayed.");
 		}
 
-		//exit testing browser
-		driver.Quit();
-	}
+        //exit testing browser
+        driver.Quit();
+    }
 
-	private static void RedMessage(string message)
-	{
-		Console.ForegroundColor = ConsoleColor.Red;
-		Console.WriteLine(message);
-		Console.ResetColor();
-	}
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
 
-	private static void GreenMessage(string message)
-	{
-		Console.ForegroundColor = ConsoleColor.Green;
-		Console.WriteLine(message);
-		Console.ResetColor();
-	}
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
 }
